5b08096c7e4673823e857170684d2e3a16140efa
Kelly Sovacool <kellysovacool@gmail.com>
2019-08-29 13:32:40 -0400

    Compile PDFs only if jupyter notebooks were changed

--- .circleci/compile.sh
+++ .circleci/compile.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
 set -e
-jupyter nbconvert --to pdf --output-dir='Lessons/Keys/pdf' Lessons/Keys/*.ipynb
-jupyter nbconvert --to pdf --output-dir='Practices/Keys/pdf' Practices/Keys/*.ipynb
-git config user.email "kellysovacool@gmail.com"
-git config user.name "Kelly Sovacool"
-git add Lessons/Keys/pdf Practices/Keys/pdf
-git commit -m "Compile PDFs [ci skip]"
-git push
+
+function install() {
+    sudo apt-get update
+    sudo apt-get install texlive-xetex pandoc
+    sudo pip install jupyter
+}
+function compile() {
+    last_compile_commit=$(git log --oneline | grep "Compile PDFs " | head -n 1 | cut -f 1 -d " ")
+    notebooks=$(git diff --name-only $last_compile_commit HEAD | grep "Keys.*ipynb")
+    for file in $notebooks
+    do
+        outdir=$(echo $file | sed "s|\(^.*/\).*$|\1|")pdf/
+        jupyter nbconvert --to pdf --output-dir=$outdir $file
+    done
+}
+function commit() {
+    git config user.email "kellysovacool@gmail.com"
+    git config user.name "Kelly Sovacool"
+    git add Lessons/Keys/pdf Practices/Keys/pdf
+    if [ $(git diff --staged --name-only) ]
+    then
+        git commit -m "Compile PDFs [ci skip]"
+        git push
+    fi
+}
+
+install
+compile
+commit
--- .circleci/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-sudo apt-get update
-sudo apt-get install texlive-xetex pandoc
-sudo pip install jupyter
