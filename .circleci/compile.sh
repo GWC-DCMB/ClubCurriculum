e1e4cc6c661fd0fce3902902ce16d50309b3a749
Kelly Sovacool <kellysovacool@gmail.com>
2019-08-29 12:16:05 -0400

    Fix YAML formatting & breakout jobs to external scripts

---
+++ .circleci/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+jupyter nbconvert --to pdf --output-dir='Lessons/Keys/pdf' Lessons/Keys/*.ipynb
+jupyter nbconvert --to pdf --output-dir='Practices/Keys/pdf' Practices/Keys/*.ipynb
+git config user.email "kellysovacool@gmail.com"
+git config user.name "Kelly Sovacool"
+git add Lessons/Keys/pdf Practices/Keys/pdf
+git commit -m "Compile PDFs [ci skip]"
+git push
---
+++ .circleci/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install texlive-xetex pandoc
+sudo pip install jupyter
